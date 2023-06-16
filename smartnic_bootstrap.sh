5d0e1820714d83d8b3bff01a461b4b09cb95e483
Philipp <phila775@gmail.com>
2023-06-17 05:04:01 +0700

    All goes to install log

--- smartnic_bootstrap.sh
+++ smartnic_bootstrap.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-sudo echo -e "\n\n ============ INSTALLATION IS IN PROGRESS =========== " |sudo tee /etc/motd
+{
+sudo echo -e "\n\n ============ INSTALLATION IS IN PROGRESS =========== " | sudo tee /etc/motd
 sudo date | sudo tee -a /etc/motd
 
-cat /local/repository/source/bashrc_template |sudo tee  /root/.bashrc
+cat /local/repository/source/bashrc_template | sudo tee /root/.bashrc
 
-DEBIAN_FRONTEND=noninteractive sudo apt-get update 2>&1
+DEBIAN_FRONTEND=noninteractive sudo apt-get update
 
-sudo echo -e "\nInstalling xfce and vnc server..." | sudo tee /opt/install_log
+sudo echo -e "\nInstalling xfce and vnc server..."
 DEPS="tightvncserver lightdm lxde xfonts-base libnss3-dev firefox"
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends $DEPS
 
-sudo echo -e "\nInstalling DOCA SDKMANAGER dependencies..." | sudo tee -a /opt/install_log
+sudo echo -e "\nInstalling DOCA SDKMANAGER dependencies..."
 DOCA_SDK_MAN_DEP="gconf-service gconf-service-backend gconf2-common libcanberra-gtk-module libcanberra-gtk0 libgconf-2-4"
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends $DOCA_SDK_MAN_DEP
 
 #setting up extra storage
-sudo echo -e "\nSet permissions for /mydata" | sudo tee -a /opt/install_log
+sudo echo -e "\nSet permissions for /mydata"
 sudo chmod -R 777 /mydata
 
-sudo echo -e "\nInstalling DOCKER and its dependencies..." | sudo tee -a /opt/install_log
+sudo echo -e "\nInstalling DOCKER and its dependencies..."
 #install dependencies for docker
 DOCKER_DEP="apt-transport-https ca-certificates curl gnupg lsb-release"
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends $DOCKER_DEP
@@ -35,7 +36,7 @@ DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends $
 DPDK_DEP="libc6-dev libpcap0.8 libpcap0.8-dev libpcap-dev meson ninja-build libnuma-dev"
 DEBIAN_FRONTEND=noninteractive sudo apt-get install -y --no-install-recommends $DPDK_DEP
 
-sudo echo -e "\nStopping docker daemon and update location for downloading sources..."  | sudo tee -a /opt/install_log
+sudo echo -e "\nStopping docker daemon and update location for downloading sources..."
 sudo /etc/init.d/docker stop
 #define new location in docker daemon.json
 sudo echo -e "{\n\t\"data-root\":\"/mydata/docker\"\n}" |sudo tee /etc/docker/daemon.json
@@ -45,40 +46,40 @@ rsync -aP /var/lib/docker/ /mydata/docker
 /etc/init.d/docker restart
 
 
-sudo echo -e "\nInstalling MLNX driver..." |sudo tee -a /opt/install_log
-#sudo echo -e "\nCopy to /opt..." >> /opt/install_log
+sudo echo -e "\nInstalling MLNX driver..."
+#sudo echo -e "\nCopy to /opt..."
 cd /opt
-sudo echo -e "\nDownloading driver to /opt..." |sudo tee -a /opt/install_log
+sudo echo -e "\nDownloading driver to /opt..."
 sudo wget  http://www.mellanox.com/downloads/ofed/MLNX_OFED-5.3-1.0.0.1/MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu20.04-x86_64.tgz
 #sudo cp /local/repository/source/MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu20.04-x86_64.tgz /opt
 #sudo cd /opt
-sudo echo -e "\nUncompress..." |sudo tee -a /opt/install_log
-sudo tar -xzvf MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu20.04-x86_64.tgz | sudo tee -a /opt/install_log
+sudo echo -e "\nUncompress..."
+sudo tar -xzvf MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu20.04-x86_64.tgz
 
 cd /opt/MLNX_OFED_LINUX-5.3-1.0.0.1-ubuntu20.04-x86_64/
-sudo echo -e "\nInstall driver..." |sudo tee -a /opt/install_log
-sudo ./mlnxofedinstall --auto-add-kernel-support --without-fw-update --force |sudo tee -a /opt/install_log
+sudo echo -e "\nInstall driver..."
+sudo ./mlnxofedinstall --auto-add-kernel-support --without-fw-update --force
 cd ..
 
-sudo echo -e "\nEnable openibd" |sudo tee -a /opt/install_log
-sudo /etc/init.d/openibd restart |sudo tee -a /opt/install_log
+sudo echo -e "\nEnable openibd"
+sudo /etc/init.d/openibd restart
 
-sudo echo -e "\nEnable rshim" |sudo tee -a /opt/install_log
+sudo echo -e "\nEnable rshim"
 sudo systemctl enable rshim
 sudo systemctl start rshim
-sudo systemctl status rshim |sudo tee -a /opt/install_log
+sudo systemctl status rshim
 
 
 sudo echo "DISPLAY_LEVEL 1" |sudo tee /dev/rshim0/misc
 
-sudo echo -e "\nUpdate netplan to assign IP to tmfif_net0..." |sudo tee -a /opt/install_log
+sudo echo -e "\nUpdate netplan to assign IP to tmfif_net0..."
 sudo cp /local/repository/source/01-netcfg.yaml /etc/netplan/
 sudo systemctl restart systemd-networkd
 sudo netplan apply
 
-sudo ifconfig tmfifo_net0 |sudo tee -a /opt/install_log
+sudo ifconfig tmfifo_net0
 
-sudo echo -e "\nEnable IP forwarding for the SmartNIC" |sudo tee -a /opt/install_log
+sudo echo -e "\nEnable IP forwarding for the SmartNIC"
 sudo echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
 sudo iptables -t nat -A POSTROUTING -o eno1 -j MASQUERADE
 sudo iptables -A FORWARD -o eno1 -j ACCEPT
@@ -93,46 +94,47 @@ done
 echo -e "\n\nTo change mode: mlxconfig -d /dev/mst/mt41686_pciconf0 s INTERNAL_CPU_MODEL=1"
 
 
-sudo echo -e "\nInstalling DPDK..." | sudo tee -a /opt/install_log
+sudo echo -e "\nInstalling DPDK..."
 cd /opt
 sudo wget https://fast.dpdk.org/rel/dpdk-20.11.1.tar.xz
-sudo tar -xJvf dpdk-20.11.1.tar.xz |sudo tee -a /opt/install_log
+sudo tar -xJvf dpdk-20.11.1.tar.xz
 cd dpdk-stable-20.11.1
 export RTE_SDK=/opt/dpdk-stable-20.11.1
 export RTE_TARGET=x86_64-native-linuxapp-gcc
 #export RTE_TARGET=arm64-armv8-linuxapp-gcc <-- this would be for the Bluefield, but now we are on the host
-sudo meson -Dexamples=all build |sudo tee -a /opt/install_log
-sudo ninja -C build | sudo tee -a /opt/install_log
-sudo ninja -C build install | sudo tee -a /opt/install_log
+sudo meson -Dexamples=all build
+sudo ninja -C build
+sudo ninja -C build install
 
-sudo echo -e "\nInstalling pktgen..." | sudo tee -a /opt/install_log
+sudo echo -e "\nInstalling pktgen..."
 cd /opt
 sudo wget https://git.dpdk.org/apps/pktgen-dpdk/snapshot/pktgen-dpdk-pktgen-21.02.0.tar.xz
-sudo tar -xJvf pktgen-dpdk-pktgen-21.02.0.tar.xz | sudo tee -a /opt/install_log
+sudo tar -xJvf pktgen-dpdk-pktgen-21.02.0.tar.xz
 cd pktgen-dpdk-pktgen-21.02.0/
-sudo make | sudo tee -a /opt/install_log
+sudo make
 sudo ldconfig
 
-sudo echo -e "\nEnabling hugepages..." | sudo tee -a /opt/install_log
+sudo echo -e "\nEnabling hugepages..."
 sudo echo 1024 |sudo tee /sys/kernel/mm/hugepages/hugepages-2048kB/nr_hugepages
 mountpoint -q /dev/hugepages || mount -t hugetlbfs nodev /dev/hugepages
 
 # FORBIDDEN - HAVE TO BE LOGGED IN :(
 # cd ..
-# sudo echo -e "\nInstall mlnx-dpdk..." |sudo tee -a /opt/install_log
+# sudo echo -e "\nInstall mlnx-dpdk..."
 # wget https://developer.nvidia.com/networking/secure/doca-sdk/DOCA_1.0/DOCA_10_b163/ubuntu2004/mlnx-dpdk_20.11-1mlnx1_amd64.deb
 # wget https://developer.nvidia.com/networking/secure/doca-sdk/DOCA_1.0/DOCA_10_b163/ubuntu2004/mlnx-dpdk-dev_20.11-1mlnx1_amd64.deb
 # DEBIAN_FRONTEND=noninteractive dpkg --force -i mlnx-dpdk_20.11-1mlnx1_amd64.deb
 
 
-# sudo echo -e "\nInstall rxptools and dpi tools..." |sudo tee -a /opt/install_log
+# sudo echo -e "\nInstall rxptools and dpi tools..."
 # wget https://developer.nvidia.com/networking/secure/doca-sdk/DOCA_1.0/DOCA_10_b163/ubuntu1804_ubuntu2004/rxp-compiler_21.02.3_amd64.deb
 # wget https://developer.nvidia.com/networking/secure/doca-sdk/DOCA_1.0/DOCA_10_b163/ubuntu2004/rxpbench_21.03_20210401_0_ubuntu_20_amd64.deb
 # wget https://developer.nvidia.com/networking/secure/doca-sdk/DOCA_1.0/DOCA_10_b163/ubuntu2004/doca-dpi-tools_21.03.038-1_amd64.deb
 
 
 
-sudo echo -e "\n\n ============ DONE =========== " |sudo tee -a /opt/install_log
-sudo echo -e "\n\n ============ INSTALLATION FINISHED =========== " |sudo tee -a /etc/motd
+sudo echo -e "\n\n ============ DONE =========== "
+sudo echo -e "\n\n ============ INSTALLATION FINISHED =========== "
 sudo date | sudo tee -a /etc/motd
 
+} 2>&1 | sudo tee /opt/install_log
